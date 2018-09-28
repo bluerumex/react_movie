cc41d1ebb69e4257902ec1d391287187099adaed
bluerumex <bluerumex@naver.com>
2018-09-28 10:28:31 +0900

    stateless function 변경

--- src/App.js
+++ src/App.js
@@ -2,40 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-/* 
-const movieTitles = [
-  "Matrix",
-  "Full Metal Jacket",
-  "Old Boy",
-  "Start wars"
-]
-
-const movieImages = [
-  "http://ticketimage.interpark.com/Movie/still_image/V16/V1601447p_s01.gif",
-  "http://image.cine21.com/cine21/poster/2005/0607/M0010102_.jpg",
-  "http://image.cine21.com/cine21/poster/2013/1114/10_42_00__52842a68c7fda.jpg",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfpffCAnV8696o7ppd4KB3rOdUOsLFbu_PeOqWWqA5FZXvYW9b"
-]
-*/
-
 class App extends Component {
   // Render: componentWillMount() -> Render() -> componentDidMount()
   // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate() 
   
-  state = {
-
-  }
+  state = {};
 
   componentDidMount() {
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-  }
+    this._getMovies();
+  };
 
   _renderMovies = () => { 
     const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
     })
     return movies;
-  }
+  };
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  };
+
+  _callApi = () => {
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err));
+  };
 
   render() {
     return (
@@ -43,7 +39,7 @@ class App extends Component {
         {this.state.movies ? this._renderMovies() : 'Loading...'}
       </div>
     )
-  }
+  };
 }
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -2,33 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Movie.css'
 
-/* 
-class Movie extends Component {
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired 
-    }
-
-    render() {
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
-}
-
-class MoviePost extends Component {
-    render() {
-      return (
-        <img alt="poster" src={this.props.poster} />
-      )
-    }
-}
-*/
-
 function Movie({title, poster}) {
     return (
         <div>
